d9211faf2be6986c2d6104e9f530fa5cd706d1de
Lautaro <lautacenizo@gmail.com>
2024-10-24 11:38:33 -0300

    first commit

---
+++ src/main/java/fr/codecake/spotifyclone/SpotifyCloneBackApplication.java
@@ -0,0 +1,15 @@
+package fr.codecake.spotifyclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class SpotifyCloneBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCloneBackApplication.class, args);
+	}
+
+}
